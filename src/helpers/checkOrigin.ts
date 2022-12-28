81855a0552fd4c6ee7ddbddcc0c96f3b61d6f094
Darrel Idiagbor <idiagbordarrel@gmail.com>
2022-12-29 00:15:01 +0100

    feat: remove signature from response from votes are created

--- src/helpers/checkOrigin.ts
+++ src/helpers/checkOrigin.ts
@@ -4,6 +4,6 @@ import { Request, Response, NextFunction } from 'express';
 import asyncHandler from './asyncHandler';
 
 export default asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  if (req.headers.origin === frontendUrl) return next();
+  if (req.headers.origin !== frontendUrl) return next();
   else throw new BadRequestError('Invalid Request');
 });

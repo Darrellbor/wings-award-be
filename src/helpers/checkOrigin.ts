c95fab8e70545d07b8d898a3c1d7b8226f29b33b
Darrel Idiagbor <idiagbordarrel@gmail.com>
2022-12-29 02:49:04 +0100

    feat: allow the check origin on dev mode

--- src/helpers/checkOrigin.ts
+++ src/helpers/checkOrigin.ts
@@ -1,9 +1,9 @@
-import { frontendUrl } from '../config';
+import { environment, frontendUrl } from '../config';
 import { BadRequestError } from '../core/ApiError';
 import { Request, Response, NextFunction } from 'express';
 import asyncHandler from './asyncHandler';
 
 export default asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-  if (req.headers.origin === frontendUrl) return next();
+  if (req.headers.origin === frontendUrl || environment === 'development') return next();
   else throw new BadRequestError('Invalid Request');
 });
